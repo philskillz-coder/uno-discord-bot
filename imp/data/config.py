02a3fce3df73d75d31795590dbdb93aaa1d7f268
philskillz-coder <76884393+philskillz-coder@users.noreply.github.com>
2022-03-20 18:34:35 +0100

    Update config.py

--- imp/data/config.py
+++ imp/data/config.py
@@ -1,6 +1,6 @@
 from discord import Object
 
 class Config:
-    token: str = "token"
+    token: str = "OTUyOTU3MTU5MDY5NjA1OTAw.Yi9kgw.TZNM3i0j8GWaX4GQQQ3phW1UaTQ"
     # guilds = [Object(id=940670548504117318)]
     guilds = [Object(id=857970291819937802), Object(id=940670548504117318)]
